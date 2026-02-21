eac628df64d139793bcef537841d0000ba3b8dc9
Tessa <tessaroberts@cmail.carleton.ca>
2026-02-20 23:57:40 -0500

    Added Race Mechanics

---
+++ Race.py
@@ -0,0 +1,158 @@
+import pygame
+import os
+#-------------------------------------------------------------------
+# INIT(IALIZE)
+#-------------------------------------------------------------------
+pygame.init()
+
+# SCREEN DIMENSIONSss
+WIDTH, HEIGHT = 960, 450
+FPS = 60 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Maze Mechanics - BETA")
+clock = pygame.time.Clock()
+
+"""
+UI to add:
+- 3 Second Start Countdown 
+- Distance counter in metres (??) --> Maybe not since we see the start to end
+- Obstacles for Player (challenging)
+- Have kid take "rests" to allow player to catch up
+"""
+
+
+# ===========================================================
+#      ADDING IMAGES --- ADD IMAGE RECT FOR BORDER COLLISION
+# ===========================================================
+
+# Defining Player
+player_image = pygame.image.load(os.path.join("ProjectImages","Arrow_R.png"))
+player_image = pygame.transform.smoothscale(player_image, (45, 45))
+
+# Player's location and speed
+player_x = 10           # Spawnpoint
+player_y = HEIGHT - 320 # Spawnpoint
+player_speed = 2
+
+# Player Rect --> needed for border collision
+p_rect = pygame.Rect(player_x, player_y, player_image.get_width(), player_image.get_height())
+
+
+# Defining Kid
+kid_image = pygame.image.load(os.path.join("ProjectImages", "kid_img.png"))
+kid_image = pygame.transform.smoothscale(kid_image, (65,65))
+
+# Kid's speed
+kid_speed = 1
+
+# Kid Rect ---> (x, y, kid)
+kid_rect = pygame.Rect(0, HEIGHT - 150, kid_image.get_width(), kid_image.get_height())
+
+# Adding BG Images
+background = pygame.image.load(os.path.join("ProjectImages", "OpenBG.jpg"))
+background = pygame.transform.smoothscale(background, (WIDTH, HEIGHT))
+#----------------------------------------------------------------------
+# Goal
+goal_image = pygame.image.load(os.path.join("ProjectImages", "exit_star.png"))
+goal_image = pygame.transform.smoothscale(goal_image, (50, 50))
+
+# Player's Lane
+goalP_rect = pygame.Rect(850, player_y, goal_image.get_width(), goal_image.get_height())
+
+# Kid's Lane
+goalK_rect = pygame.Rect(850, HEIGHT- 150, goal_image.get_width(), goal_image.get_height())
+
+# ---------------------------------------------------------------------
+#-------------------------------------------------------------------------
+
+# ======================
+#      GAME LOOP LOGIC
+# ======================
+
+end = False
+
+# Player movement Variables
+player_dx = 0 #--> Direction x
+player_dy = 0 #--> Direction y
+
+
+# =========================================
+#      GAME LOGIC: Updates player position
+# =========================================
+while not end:
+    # Event handling: Check for user input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            end = True
+
+        # EXIT KEY - "Esc"
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: 
+                end = True
+            # RIGHT
+            if event.key == pygame.K_d:
+                player_dx = player_speed
+        
+        if event.type == pygame.KEYUP:
+                if event.key in (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s):
+                    player_dx = 0
+                    player_dy = 0
+    
+    p_rect.x += player_dx 
+    p_rect.y += player_dy
+
+    #Detecting who touches Goal first
+    FinishLine_x = 850
+
+    # Detecting who touches Goal first
+    if p_rect.colliderect(goalP_rect):
+        print("You win!")
+        end = True
+
+    if kid_rect.colliderect(goalK_rect):
+        print("You lose!")
+        end = True
+
+    # Auto moves on X axis
+    kid_rect.x += kid_speed
+
+    # BORDER COLLISION --> use < & > adds flexibility, to snap ONLY if player is too far
+    # Player
+    if p_rect.left < 6:
+        p_rect.left = 6
+
+    if p_rect.top < 7:
+        p_rect.top = 7
+       
+    if p_rect.bottom > HEIGHT - 6:
+        p_rect.bottom = HEIGHT - 6
+
+    if p_rect.right > WIDTH - 7:
+        p_rect.right = WIDTH - 7
+
+    # Kid
+    if kid_rect.left < 6:
+        kid_rect.left = 6
+
+    if kid_rect.top < 7:
+        p_rect.top = 7
+       
+    if kid_rect.bottom > HEIGHT - 6:
+        p_rect.bottom = HEIGHT - 6
+
+    if kid_rect.right > WIDTH - 7:
+        kid_rect.right = WIDTH - 7
+   #---------------------------------------------------------------
+    # DRAW
+    screen.blit(background, (0, 0))
+    screen.blit(player_image, p_rect)
+    screen.blit(kid_image, kid_rect)
+    screen.blit(goal_image, goalP_rect) 
+    screen.blit(goal_image, goalK_rect)
+    #>>---------------------------------------------------------------
+    # C. Update the display and cap frame rate
+    pygame.display.flip()
+    clock.tick(FPS)
+
+# Quit Pygame
+pygame.quit()
---
+++ pygame_button.py
@@ -0,0 +1,31 @@
+"""
+Buttons in pygame: https://thepythoncode.com/article/make-a-button-using-pygame-in-python
+
+WIP:
+"""
+
+import pygame
+
+import os
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 640, 480
+FPS = 60 # Increased FPS for smoother movement
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Button Test")
+clock = pygame.time.Clock()
+# Adding font
+font = pygame.font.SysFont("Calibri", 80)
+
+
+class Buttons():
+    # Define Images
+    button_img = pygame.image.load(os.path.join("ProjectImages", "ButtonImage.png"))
+    button_img = pygame.transform.smoothscale(button_img, (150, 60))  # resize if needed
+    button_rect = button_img.get_rect(center=(WIDTH//2, HEIGHT//2))
+
+    # Detecting mouseclicks --> similar to keydowns
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        print("you clicked the button")
