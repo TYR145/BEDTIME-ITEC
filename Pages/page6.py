f3e93f55d0928b3317d0a816b1101f9913ed31fc
Tessa <tessaroberts@cmail.carleton.ca>
2026-03-15 01:47:17 -0400

    Changed 'd' to 'r' for restart from page 6

--- Pages/page6.py
+++ Pages/page6.py
@@ -2,15 +2,15 @@
 
 def page6():
     print("=== PAGE 6 ===")
-    print("Press D to go back to Page 5.")
-    print("Press A to restart at page 1")
+    print("Press A to go back to Page 5.")
+    print("Press R to restart at page 1")
     #print("Press P to play [BOOKSEARCH - N/A])
 
     choice = input("Choose: ").lower()
 
-    if choice == "d":
+    if choice == "a":
         return "page5"
-    elif choice == "a":
+    elif choice == "r": # Changed from "d" to "r" to keep "a" for going back to prvious page
         return "page1"
     #elif choice == "p":
         #BOOKSEARCH.run()
